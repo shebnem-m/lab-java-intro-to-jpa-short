e1c3854508f49b01044c1ffc59542fc41632ec57
Shebnem.M <snmmrdv@gmail.com>
2026-04-02 15:50:25 +0400

    update again intro jpa lab

--- lab_jpa/src/main/java/org/example/lab_jpa/repositories/FlightBookingRepository.java
+++ lab_jpa/src/main/java/org/example/lab_jpa/repositories/FlightBookingRepository.java
@@ -1,7 +1,13 @@
 package org.example.lab_jpa.repositories;
 
+import org.example.lab_jpa.models.Customer;
+import org.example.lab_jpa.models.Flight;
 import org.example.lab_jpa.models.FlightBooking;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface FlightBookingRepository extends JpaRepository<FlightBooking, Integer> {
+     List<FlightBooking> findByCustomerId(Integer customerId);
+     List<FlightBooking> findByFlightId(Integer flightId);
 }
